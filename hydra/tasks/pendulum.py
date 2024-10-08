cff4d9f462b335db985a80257f829ea7258a03ff
Vince Kurtz <vjkurtz@gmail.com>
2024-10-08 09:32:25 -0500

    define full pendulum task

--- hydra/tasks/pendulum.py
+++ hydra/tasks/pendulum.py
@@ -1,4 +1,5 @@
 import jax
+import jax.numpy as jnp
 import mujoco
 from mujoco import mjx
 
@@ -16,6 +17,7 @@ class Pendulum(Task):
         )
 
         sim_steps_per_control_step = 5
+        self.dt = mj_model.opt.timestep * sim_steps_per_control_step
 
         super().__init__(
             mjx.put_model(mj_model),
@@ -25,8 +27,11 @@ class Pendulum(Task):
 
     def running_cost(self, state: mjx.Data, control: jax.Array) -> jax.Array:
         """The running cost ℓ(xₜ, uₜ) penalizes large torques."""
-        pass
+        return 0.01 * self.dt * jnp.sum(jnp.square(control))
 
     def terminal_cost(self, state: mjx.Data) -> jax.Array:
         """The terminal cost ϕ(x_T) penalizes distance from the upright."""
-        pass
+        theta = state.qpos[0] - jnp.pi
+        theta_dot = state.qvel[0]
+        theta_err = jnp.array([jnp.cos(theta) - 1, jnp.sin(theta)])
+        return jnp.sum(jnp.square(theta_err)) + 0.1 * jnp.square(theta_dot)
--- tests/test_pendulum.py
+++ tests/test_pendulum.py
@@ -1,3 +1,6 @@
+import jax.numpy as jnp
+from mujoco import mjx
+
 from hydra.tasks.pendulum import Pendulum
 
 
@@ -5,6 +8,16 @@ def test_pendulum() -> None:
     """Make sure we can instantiate the Pendulum task."""
     task = Pendulum()
     assert isinstance(task, Pendulum)
+
+    state = mjx.make_data(task.model)
+    assert isinstance(state, mjx.Data)
+
+    ell = task.running_cost(state, jnp.zeros(1))
+    assert ell.shape == ()
+
+    phi = task.terminal_cost(state)
+    assert phi.shape == ()
+    assert phi > 0.0
 
 
 if __name__ == "__main__":
