52ada5c4387d9d940176dd14382881fc6018e5c0
Vince Kurtz <vjkurtz@gmail.com>
2024-10-08 16:13:18 -0500

    tune the pendulum example

--- hydra/tasks/pendulum.py
+++ hydra/tasks/pendulum.py
@@ -34,13 +34,13 @@ class Pendulum(Task):
     def running_cost(self, state: mjx.Data, control: jax.Array) -> jax.Array:
         """The running cost ℓ(xₜ, uₜ)."""
         theta_cost = self._distance_to_upright(state)
-        theta_dot_cost = 0.1 * jnp.square(state.qvel[0])
-        control_cost = 0.01 * jnp.sum(jnp.square(control))
+        theta_dot_cost = 0.01 * jnp.square(state.qvel[0])
+        control_cost = 0.001 * jnp.sum(jnp.square(control))
         total_cost = theta_cost + theta_dot_cost + control_cost
         return self.dt * total_cost
 
     def terminal_cost(self, state: mjx.Data) -> jax.Array:
         """The terminal cost ϕ(x_T)."""
         theta_cost = self._distance_to_upright(state)
-        theta_dot_cost = 0.1 * jnp.square(state.qvel[0])
+        theta_dot_cost = 0.01 * jnp.square(state.qvel[0])
         return theta_cost + theta_dot_cost
