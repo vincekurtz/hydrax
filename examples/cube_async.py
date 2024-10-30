f803f22230ab08796936e95dec1f5cda15f64df0
Vince Kurtz <vjkurtz@gmail.com>
2024-10-30 11:22:25 -0500

    [WIP] getting rid of ground plane fixes cuda error

--- examples/cube_async.py
+++ examples/cube_async.py
@@ -12,30 +12,26 @@ Asynchronous simulation is more realistic than deterministic simulation, but
 offers limited features (e.g., no trace visualization).
 """
 
+if __name__ == "__main__":
+    # Asynchronous simulations must be wrapped in a __main__ block, see
+    # https://docs.python.org/3/library/multiprocessing.html
 
-# For asynchronous simulation, we need to define the task and controller inside
-# a function. This ensures that all jax code is isolated in one process. Any
-# jax code outside that process will result in various inscrutable `os.fork()`
-# and `CUDA_ERROR_NOT_INITIALIZED` errors.
-def make_controller() -> PredictiveSampling:
-    """Set up the controller."""
+    # Define the task and controller
     task = CubeRotation()
-    return PredictiveSampling(
-        task, num_samples=32, noise_level=0.2, num_randomizations=32
+
+    ctrl = PredictiveSampling(task, num_samples=32, noise_level=0.2, num_randomizations=32)
+
+    # Define the model used for simulation (with more realistic parameters)
+    mj_model = mujoco.MjModel.from_xml_path(ROOT + "/models/cube/scene.xml")
+    mj_model.opt.timestep = 0.002
+    mj_model.opt.iterations = 100
+    mj_model.opt.ls_iterations = 50
+    mj_model.opt.cone = mujoco.mjtCone.mjCONE_ELLIPTIC
+    mj_data = mujoco.MjData(mj_model)
+
+    # Run the interactive simulation
+    run_interactive(
+        ctrl,
+        mj_model,
+        mj_data,
     )
-
-
-# Define the model used for simulation (with more realistic parameters)
-mj_model = mujoco.MjModel.from_xml_path(ROOT + "/models/cube/scene.xml")
-mj_model.opt.timestep = 0.002
-mj_model.opt.iterations = 100
-mj_model.opt.ls_iterations = 50
-mj_model.opt.cone = mujoco.mjtCone.mjCONE_ELLIPTIC
-mj_data = mujoco.MjData(mj_model)
-
-# Run the interactive simulation
-run_interactive(
-    make_controller,
-    mj_model,
-    mj_data,
-)
