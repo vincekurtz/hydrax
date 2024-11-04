b42be816c0d17d3624eb1bea3a80f37f04dd5a98
Vince Kurtz <vjkurtz@gmail.com>
2024-11-04 14:38:56 -0600

    make cart-pole task

---
+++ tests/test_cart_pole.py
@@ -0,0 +1,27 @@
+import jax.numpy as jnp
+from mujoco import mjx
+
+from hydrax.tasks.cart_pole import CartPole
+
+
+def test_cart_pole() -> None:
+    """Make sure we can instantiate the task."""
+    task = CartPole()
+    assert isinstance(task, CartPole)
+
+    state = mjx.make_data(task.model)
+    assert isinstance(state, mjx.Data)
+
+    ell = task.running_cost(state, jnp.zeros(1))
+    assert ell.shape == ()
+
+    phi = task.terminal_cost(state)
+    assert phi.shape == ()
+    assert phi > 0.0
+
+    y = task.get_obs(state)
+    assert y.shape == (4,)
+
+
+if __name__ == "__main__":
+    test_cart_pole()
