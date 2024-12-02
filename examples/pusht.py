8795ebbf6e6a165778920abf1168aee97cbf1bcd
Vince Kurtz <vjkurtz@gmail.com>
2024-12-02 12:28:41 -0600

    refine dynamics to reduce passthrough

--- examples/pusht.py
+++ examples/pusht.py
@@ -12,12 +12,13 @@ Run an interactive simulation of the push-T task with predictive sampling.
 task = PushT()
 
 # Set up the controller
-ctrl = PredictiveSampling(task, num_samples=1024, noise_level=0.5)
+ctrl = PredictiveSampling(task, num_samples=1024, noise_level=0.4)
 
 # Define the model used for simulation
 mj_model = task.mj_model
 mj_model.opt.timestep = 0.001
 mj_model.opt.iterations = 100
+mj_model.opt.ls_iterations = 50
 mj_data = mujoco.MjData(mj_model)
 
 # Run the interactive simulation
