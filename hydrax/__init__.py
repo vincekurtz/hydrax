3935cf9a5cc2c6432c315b3294ab13cb3322723d
Vince Kurtz <vjkurtz@gmail.com>
2025-01-16 11:18:45 -0600

    set XLA flags in __init__.py

--- hydrax/__init__.py
+++ hydrax/__init__.py
@@ -1,4 +1,9 @@
+import os
 from pathlib import Path
 
 # package root
 ROOT = str(Path(__file__).parent.absolute())
+
+# Set XLA flags for better performance
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.9"
+os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=true "
