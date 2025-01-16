17a6d3ce9d978817cf82a11c3a463d76226a408c
Vince Kurtz <vjkurtz@gmail.com>
2025-01-16 11:42:54 -0600

    add async option for humanoid standup

--- examples/humanoid.py
+++ examples/humanoid.py
@@ -1,6 +1,9 @@
+import argparse
+
 import mujoco
 
 from hydrax.algs import MPPI
+from hydrax.simulation.asynchronous import run_interactive as run_async
 from hydrax.simulation.deterministic import run_interactive
 from hydrax.tasks.humanoid import Humanoid
 
@@ -8,33 +11,64 @@ from hydrax.tasks.humanoid import Humanoid
 Run an interactive simulation of the humanoid task.
 """
 
-# Define the task (cost and dynamics)
-task = Humanoid()
+# Need to be wrapped in main loop for async simulation
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(
+        description="Run an interactive simulation of humanoid (G1) standup."
+    )
+    parser.add_argument(
+        "-a",
+        "--asynchronous",
+        action="store_true",
+        help="Use asynchronous simulation",
+        default=False,
+    )
+    args = parser.parse_args()
 
-# Set up the controller
-ctrl = MPPI(
-    task,
-    num_samples=128,
-    noise_level=1.0,
-    temperature=0.1,
-    num_randomizations=4,
-)
+    # Define the task (cost and dynamics)
+    task = Humanoid()
 
-# Define the model used for simulation
-mj_model = task.mj_model
-mj_model.opt.timestep = 0.01
+    # Set up the controller
+    ctrl = MPPI(
+        task,
+        num_samples=128,
+        noise_level=1.0,
+        temperature=0.1,
+        num_randomizations=4,
+    )
 
-# Set the initial state so the robot falls and needs to stand back up
-mj_data = mujoco.MjData(mj_model)
-mj_data.qpos[:] = mj_model.keyframe("stand").qpos
-mj_data.qpos[3:7] = [0.7, 0.0, 0.7, 0.0]
+    # Define the model used for simulation
+    mj_model = task.mj_model
+    mj_model.opt.timestep = 0.01
 
-# Run the interactive simulation
-run_interactive(
-    ctrl,
-    mj_model,
-    mj_data,
-    frequency=50,
-    show_traces=False,
-    max_traces=1,
-)
+    # Set the initial state so the robot falls and needs to stand back up
+    mj_data = mujoco.MjData(mj_model)
+    mj_data.qpos[:] = mj_model.keyframe("stand").qpos
+    mj_data.qpos[3:7] = [0.7, 0.0, 0.7, 0.0]
+
+    # Run the interactive simulation
+    if args.asynchronous:
+        print("Running asynchronous simulation")
+
+        # Tighten up the simulator parameters, since it's running on CPU and
+        # therefore won't slow down the planner
+        mj_model.opt.timestep = 0.005
+        mj_model.opt.iterations = 100
+        mj_model.opt.ls_iterations = 50
+        mj_model.opt.cone = mujoco.mjtCone.mjCONE_ELLIPTIC
+
+        run_async(
+            ctrl,
+            mj_model,
+            mj_data,
+        )
+    else:
+        print("Running deterministic simulation")
+        run_interactive(
+            ctrl,
+            mj_model,
+            mj_data,
+            frequency=50,
+            show_traces=False,
+        )
--- hydrax/__init__.py
+++ hydrax/__init__.py
@@ -5,5 +5,5 @@ from pathlib import Path
 ROOT = str(Path(__file__).parent.absolute())
 
 # Set XLA flags for better performance
-os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.9"
+os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = "0.7"
 os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=true "
