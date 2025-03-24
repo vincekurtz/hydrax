a1ec216f764364a384c56bdaa6f083e6b800e60b
Vince Kurtz <vjkurtz@gmail.com>
2025-03-24 17:29:44 -0500

    passable run with cem

--- examples/humanoid_mocap.py
+++ examples/humanoid_mocap.py
@@ -1,9 +1,7 @@
 import mujoco
 
-from hydrax.algs import MPPI
+from hydrax.algs import CEM
 from hydrax.simulation.deterministic import run_interactive
-
-# from hydrax.tasks.humanoid_standup import HumanoidStandup
 from hydrax.tasks.humanoid_mocap import HumanoidMocap
 
 """
@@ -12,23 +10,24 @@ Run an interactive simulation of the humanoid motion capture tracking task.
 
 
 # Define the task (cost and dynamics)
-task = HumanoidMocap()
+task = HumanoidMocap(reference_filename="run1_subject2.csv")
 
 # Set up the controller
-ctrl = MPPI(
+ctrl = CEM(
     task,
     num_samples=512,
-    temperature=0.02,
-    noise_level=0.1,
+    num_elites=10,
+    sigma_start=0.1,
+    sigma_min=0.1,
 )
 
 # Define the model used for simulation
 mj_model = task.mj_model
 mj_model.opt.timestep = 0.01
-# mj_model.opt.iterations = 10
-# mj_model.opt.ls_iterations = 50
-# mj_model.opt.o_solimp = [0.9, 0.95, 0.001, 0.5, 2]
-# mj_model.opt.enableflags = mujoco.mjtEnableBit.mjENBL_OVERRIDE
+mj_model.opt.iterations = 10
+mj_model.opt.ls_iterations = 50
+mj_model.opt.o_solimp = [0.9, 0.95, 0.001, 0.5, 2]
+mj_model.opt.enableflags = mujoco.mjtEnableBit.mjENBL_OVERRIDE
 
 # Set the initial state
 mj_data = mujoco.MjData(mj_model)
