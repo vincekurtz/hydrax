2b7ef819602a4830cc73bf3e24c5e77048de5ce1
Tom0Brien <thomas16441@gmail.com>
2025-04-20 14:42:04 +1000

    Update controllers.py

--- hydrax/benchmark/controllers.py
+++ hydrax/benchmark/controllers.py
@@ -21,57 +21,56 @@ def get_default_controller_configs() -> Dict[str, Dict[str, Any]]:
         "PredictiveSampling": {
             "class": PredictiveSampling,
             "params": {
-                "num_samples": 512,
+                "num_samples": 128,
                 "noise_level": 0.1,
                 "num_randomizations": 10,
                 "plan_horizon": 0.5,
                 "spline_type": "zero",
-                "num_knots": 11,
+                "num_knots": 6,
             },
         },
         "MPPI": {
             "class": MPPI,
             "params": {
-                "num_samples": 512,
+                "num_samples": 128,
                 "noise_level": 0.1,
                 "temperature": 0.01,
                 "plan_horizon": 0.25,
                 "spline_type": "zero",
-                "num_knots": 11,
+                "num_knots": 6,
             },
         },
         "CEM": {
             "class": CEM,
             "params": {
-                "num_samples": 512,
+                "num_samples": 128,
                 "num_elites": 20,
                 "sigma_start": 0.3,
                 "sigma_min": 0.05,
                 "explore_fraction": 0.5,
                 "plan_horizon": 0.25,
                 "spline_type": "zero",
-                "num_knots": 11,
+                "num_knots": 6,
             },
         },
-        # Uncomment to include Evosax controllers
-        # "CMA_ES": {
-        #     "class": Evosax,
-        #     "params": {
-        #         "optimizer": evosax.CMA_ES,
-        #         "num_samples": 16,
-        #         "plan_horizon": 0.25,
-        #         "spline_type": "zero",
-        #         "num_knots": 11,
-        #     },
-        # },
-        # "Sep_CMA_ES": {
-        #     "class": Evosax,
-        #     "params": {
-        #         "optimizer": evosax.Sep_CMA_ES,
-        #         "num_samples": 16,
-        #         "plan_horizon": 0.25,
-        #         "spline_type": "zero",
-        #         "num_knots": 11,
-        #     },
-        # },
+        "CMA_ES": {
+            "class": Evosax,
+            "params": {
+                "optimizer": evosax.CMA_ES,
+                "num_samples": 128,
+                "plan_horizon": 0.25,
+                "spline_type": "zero",
+                "num_knots": 6,
+            },
+        },
+        "Sep_CMA_ES": {
+            "class": Evosax,
+            "params": {
+                "optimizer": evosax.Sep_CMA_ES,
+                "num_samples": 128,
+                "plan_horizon": 0.25,
+                "spline_type": "zero",
+                "num_knots": 6,
+            },
+        },
     }
