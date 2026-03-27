95856ba15728515a51af1bf709b29c5774f75b95
sesteban951 <sesteban@caltech.edu>
2026-03-26 17:38:47 -0700

    fixed the tmp jax cache directory problem. It will now make /home/user/tmp/ directory and save jax_cache there

--- hydrax/__init__.py
+++ hydrax/__init__.py
@@ -10,4 +10,6 @@ ROOT = str(Path(__file__).parent.absolute())
 os.environ["XLA_FLAGS"] = "--xla_gpu_triton_gemm_any=true "
 
 # Enable persistent compilation cache
-jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+_jax_cache_dir = os.path.expanduser("~/tmp/jax_cache")
+os.makedirs(_jax_cache_dir, exist_ok=True)
+jax.config.update("jax_compilation_cache_dir", _jax_cache_dir)
